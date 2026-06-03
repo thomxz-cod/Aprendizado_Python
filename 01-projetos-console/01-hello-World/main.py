0b3facee6aec9e9e127f3ce5ff15e1053c331de5
thomxz-cod <thomazerick35@gmail.com>
2026-06-03 18:17:12 -0300

    brincando

--- 01-projetos-console/01-hello-World/main.py
+++ 01-projetos-console/01-hello-World/main.py
@@ -1,1 +1,11 @@
-print("Hello, Wolrd!")
+print("Hello, Wolrd!") # print() imprime algo no console 
+
+# Comentario de linha unica ( explica o "porque" do codigo, não o "o quê" )
+
+"""
+
+bloco de comentario ( docstring )
+Usado para comentar bloco de codigos maiores dentro das aspas duplas ""
+
+"""
+
