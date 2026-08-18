f3e69a78c981abf6be5181f1458b87c529b91220
thomxz-cod <thomazerick35@gmail.com>
2026-08-17 22:04:20 -0300

    API GitHub

--- Trilha/modulo 1 - Nivelamento WEB/aula 5 API/atividade/site/scripts/script.js
+++ Trilha/modulo 1 - Nivelamento WEB/aula 5 API/atividade/site/scripts/script.js
@@ -1,12 +1,34 @@
-const botao = document.getElementById("botao");
-const perfil = document.getElementById("perfil");
+// const botao = document.getElementById("botao");
+// const perfil = document.getElementById("perfil");
 
-const inputUsuario = document.getElementById("usuario");
+// const inputUsuario = document.getElementById("usuario");
 
-botao.addEventListener("click", () => {
-    const nome = inputUsuario.value
-    console.log("Botão clicado")
-    fetch(`https://api.github.com/users/${nome}`)
+// botao.addEventListener("click", () => {
+//     const nome = inputUsuario.value
+//     console.log("Botão clicado")
+//     fetch(`https://api.github.com/users/${nome}`)
 
-    alert(nome)
-})
+//     alert(nome)
+// })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.getElementById("buscar");
+    
+    botao.addEventListener("click", () => {      
+        let input_nome = document.getElementById("usuario");
+        let imagem = document.getElementById("imagemPerfil");
+        let titulo = document.getElementById("titulo");
+        let bio = document.getElementById("bio");
+
+        let nome = input_nome.value;
+        
+        fetch(`https://api.github.com/users/${nome}`)
+        .then(response => response.json())
+        .then(data => {
+            imagem.src = data.avatar_url
+            titulo.innerText = data.name
+            bio.innerText = data.bio
+        })
+
+    })
+});
